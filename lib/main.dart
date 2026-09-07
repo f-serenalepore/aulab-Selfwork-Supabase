eeebc2d1b1dff2acf5c41ec082e1e5313842c30c
f-serenalepore <f.serenalepore@gmail.com>
2026-09-07 11:17:48 +0200

    aggiunta supabase_client

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_app/core/supabase_client.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await SupabaseClientManager.init();
   runApp(const MyApp());
 }
 
