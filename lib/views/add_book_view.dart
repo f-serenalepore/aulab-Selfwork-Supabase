c026cde4d491024a2a9739bdb09a0d6e683d81b1
f-serenalepore <f.serenalepore@gmail.com>
2026-09-09 11:13:02 +0200

    SubmitForm

--- lib/views/add_book_view.dart
+++ lib/views/add_book_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:supabase_app/models/book_model.dart';
+import 'package:supabase_app/viewmodel/book_view_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:uuid/uuid.dart';
 
 String _statusLabel(BookStatus status) {
   switch (status) {
@@ -42,11 +46,46 @@ class _AddBookViewState extends State<AddBookView> {
     super.dispose;
   }
 
-  void _submitForm() {
-    if (_formKey.currentState!.validate()) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Dati validi, possiamo andare avanti")),
+  void _submitForm() async {
+    if (_formKey.currentState!.validate() && _selectedStatus != null) {
+      final userId = Supabase.instance.client.auth.currentUser?.id;
+      if (userId == null) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(const SnackBar(content: Text("utente non autenticato")));
+        return;
+      }
+
+      final newBook = Book(
+        id: const Uuid().v4(), //genera un nuovo UUID
+        userId: userId,
+        title: _titleController.text.trim(),
+        genre: _genreController.text.trim(),
+        author: _authorController.text.trim(),
+        pages: int.tryParse(_pagesController.text.trim()) ?? 0,
+        rating: _rating.toInt(),
+        comment: _commentController.text.trim(),
+        status: _selectedStatus!,
+        createdAt: DateTime.now(),
       );
+
+      try {
+        await Provider.of<BookViewModel>(
+          context,
+          listen: false,
+        ).addBook(newBook);
+        if (context.mounted) {
+          Navigator.pop(context);
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Dati validi, possiamo andare avanti")),
+          );
+        }
+      } catch (e) {
+        print("Errore nel salvataggio del libro: $e");
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("Errore salvataggio libro")));
+      }
     }
   }
 
